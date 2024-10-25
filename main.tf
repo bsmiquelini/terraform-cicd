bfce2584642e38504561c4fbd5fd21ad45cdbbbe
Bruno <185800889+bsmiquelini@users.noreply.github.com>
2024-10-25 16:53:37 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,11 +3,13 @@ resource "azurerm_resource_group" "rg" {
   name     = var.rg_name
   location = var.location
 }
+# Usando o modulo https://github.com/bsmiquelini/terraform-modules.git//modules/storage_account
+module "storage_account" {
+  source = "git::https://github.com/bsmiquelini/terraform-modules.git//modules/storage_account?ref=main"
 
-resource "azurerm_storage_account" "storage" {
-  name                     = var.storageName
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = var.storageTier
-  account_replication_type = "GRS"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  name                = var.storageName
+  env                 = "Prod"
 }
+
