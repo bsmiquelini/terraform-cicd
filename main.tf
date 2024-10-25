46d277471c69fd3042db787d0a320355ae74aee0
Bruno <185800889+bsmiquelini@users.noreply.github.com>
2024-10-25 15:30:02 +0000

    Adjust variables and TF import RG (#2)
    * Fix fmt
    
    * enable ci for all branches
    
    * Fix branches to run CI
    
    * Update terraform.yml
    
    * Comment Workspace
    
    * Add Documentation
    
    * Update terraform.yml
    
    * Check vars
    
    * Update terraform.yml
    
    * Update terraform.yml
    
    * Update terraform.yml
    
    * Update terraform.yml
    
    * Add vars rg_name subscription_id and location
    
    * Update terraform.yml
    
    * Update main.tf
    
    * Add plan
    
    * Update terraform.yml
    
    ---------
    
    Co-authored-by: bruno miquelini <bruno.miquelini@msn.com>

--- main.tf
+++ main.tf
@@ -1,46 +1,62 @@
 terraform {
-  cloud { 
-    organization = "bmcent" 
-    workspaces { 
-      name = "TerraformCI" 
-    } 
-  } 
+  #cloud {
+  #  organization = "bmcent"
+  #  workspaces {
+  #    name = "TerraformCI"
+  #  }
+  #}
   required_providers {
     azurerm = {
-        version = "4.7.0"
-        source = "hashicorp/azurerm"
+      version = "4.7.0"
+      source  = "hashicorp/azurerm"
     }
   }
 }
 
 provider "azurerm" {
-    subscription_id = "80ea84e8-afce-4851-928a-9e2219724c69"
-    resource_provider_registrations = "none"
-    features {}
-  
+  subscription_id                 = var.subscription_id
+  resource_provider_registrations = "none"
+  features {}
+
+}
+
+
+variable "rg_name" {
+  type        = string
+  description = "Nome do Resource Group"
+}
+
+variable "location" {
+  type        = string
+  description = "Local do Resource Group"
+}
+
+variable "subscription_id" {
+  type        = string
+  description = "Subscription ID"
 }
 
 variable "storageName" {
-    type = string
-    default = "bsmiquelini"
-    description = "Nome do storage Account"
+  type        = string
+  default     = "bsmiquelini"
+  description = "Nome do storage Account"
 }
 
 variable "storageTier" {
-    type = string
-    default = "Standard"
-    description = "Tipo do tier do storage"
+  type        = string
+  default     = "Standard"
+  description = "Tipo do tier do storage"
 }
 
 resource "azurerm_resource_group" "rg" {
-    name = "811-0a40075a-provide-continuous-delivery-with-gith"
-    location = "South Central US"
+  name     = var.rg_name
+  location = var.location
 }
 
 resource "azurerm_storage_account" "storage" {
-    name = var.storageName
-    resource_group_name = azurerm_resource_group.rg.name
-    location = azurerm_resource_group.rg.location
-    account_tier = var.storageTier
-    account_replication_type = "GRS"
+  name                     = var.storageName
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = var.storageTier
+  account_replication_type = "GRS"
 }
